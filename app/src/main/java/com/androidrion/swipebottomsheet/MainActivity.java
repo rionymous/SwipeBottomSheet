77b1066a5b6bbfa4301e62811598cd57a7011b15
rionymous_ <callmerion24@gmail.com>
2019-11-09 05:05:52 +0700

    Initial commit

---
+++ app/src/main/java/com/androidrion/swipebottomsheet/MainActivity.java
@@ -0,0 +1,50 @@
+package com.androidrion.swipebottomsheet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior bottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = findViewById(R.id.bottom_sheet);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        bottomSheetBehavior = BottomSheetBehavior.from(linearLayout);
+
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+
+        Toast.makeText(this, "Swipe up", Toast.LENGTH_SHORT).show();
+    }
+}
